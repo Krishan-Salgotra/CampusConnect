b4b6ff55b8f965355e6434c97451c7f941f4f773
Krishan-Salgotra <krishansalgotra2006@gmail.com>
2026-06-21 16:58:02 +0530

    Connect frontend to production backend

--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://campusconnect-backend-t2hl.onrender.com/api"
 });
 
 export default api;
